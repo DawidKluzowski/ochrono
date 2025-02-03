8430cac575bc74b7ecdcd0205dd070bfb25431d2
DawidKluzowski <kluzowski.dawid@gmail.com>
2025-02-03 20:42:22 +0100

    style workload table

--- ochrono/src/components/workloadTable/index.tsx
+++ ochrono/src/components/workloadTable/index.tsx
@@ -17,26 +17,39 @@ function WorkloadTable() {
     setCurentMonthLength(getDaysInMonth(today));
   }, []);
 
-  console.log(curentMonthLength);
-
   return (
-    <div>
+    <div className="relative">
+      <div
+        className={`text-sm grid grid-cols-[repeat(${10 + curentMonthLength},_minmax(0,_1fr))] auto-cols-auto grid-flow-col overflow-x-hidden`}
+      >
+        <div className="col-span-6 p-1 m-1"></div>
+        {Array.from({ length: curentMonthLength }, (_, index) => (
+          <div
+            key={Math.random()}
+            className={`${
+              (index + 1) % 2 === 1 ? 'bg-gray-300' : 'bg-gray-400'
+            } m-4 text-center w-5 h-5 gap-3`}
+          >
+            {index + 1}
+          </div>
+        ))}
+      </div>
       {LIST_OF_EMPLOEEYS.map((employee) => {
         return (
           <div
             key={employee}
-            className=" text-sm grid grid-cols-[repeat(38,_minmax(0,_1fr))] gap-3 auto-cols-auto grid-flow-col w-full overflow-x-hidden "
+            className={`text-sm grid grid-cols-[repeat(${10 + curentMonthLength},_minmax(0,_1fr))] auto-cols-auto grid-flow-col w-full overflow-x-hidden h-6 mb-2`}
           >
-            <div className="col-span-5 bg-slate-600 p-1 text-white m-1">
+            <div className="col-span-6 bg-slate-600 text-white h-5 text-center ">
               {employee}
             </div>
             {Array.from({ length: curentMonthLength }, (_, index) => (
               <div
                 key={Math.random()}
-                className=" bg-gray-300 m-4 text-center w-5 h-5 gap-3 cursor-pointer"
-              >
-                {index + 1}
-              </div>
+                className={`${
+                  (index + 1) % 2 === 1 ? 'bg-gray-300' : 'bg-gray-400'
+                } ml-4 text-center w-5 h-5 cursor-pointer`}
+              />
             ))}
           </div>
         );
